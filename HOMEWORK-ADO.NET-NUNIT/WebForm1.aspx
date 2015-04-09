35f51218b3533f67d7836104fef2b38bc65bd4d9
1101137230 <1101137230@cc.kuas.edu.tw>
2015-04-09 19:24:25 +0800

    2

--- HOMEWORK-ADO.NET-NUNIT/WebForm1.aspx
+++ HOMEWORK-ADO.NET-NUNIT/WebForm1.aspx
@@ -11,6 +11,44 @@
     <form id="form1" runat="server">
     <div>
     
+        <asp:TextBox ID="searchBox" runat="server"></asp:TextBox>
+        <asp:Button ID="searchButton" runat="server" Text="查詢" OnClick="searchButton_Click" />
+        <br />
+        <br />
+        <asp:GridView ID="resultGridView" runat="server" CellPadding="4" ForeColor="Black" GridLines="Vertical" BackColor="White" BorderColor="#DEDFDE" BorderStyle="None" BorderWidth="1px">
+            <AlternatingRowStyle BackColor="White" />
+            <FooterStyle BackColor="#CCCC99" />
+            <HeaderStyle BackColor="#6B696B" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#F7F7DE" ForeColor="Black" HorizontalAlign="Right" />
+            <RowStyle BackColor="#F7F7DE" />
+            <SelectedRowStyle BackColor="#CE5D5A" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#FBFBF2" />
+            <SortedAscendingHeaderStyle BackColor="#848384" />
+            <SortedDescendingCellStyle BackColor="#EAEAD3" />
+            <SortedDescendingHeaderStyle BackColor="#575357" />
+        </asp:GridView>
+        <br />
+        編號:<asp:TextBox ID="insertIDTextBox" runat="server"></asp:TextBox>
+        班級:<asp:TextBox ID="insertClassTextBox" runat="server"></asp:TextBox>
+        姓名:<asp:TextBox ID="insertNameTextBox" runat="server"></asp:TextBox>
+        性別:<asp:TextBox ID="insertGenderTextBox" runat="server"></asp:TextBox> 
+        生日:<asp:TextBox ID="insertBirthdayTextBox" runat="server"></asp:TextBox> 
+        <asp:Button ID="insertButton" runat="server" OnClick="insertButton_Click" Text="新增" />
+        <br />
+        <br />
+
+        編號:<asp:TextBox ID="updateIDTextBox" runat="server"></asp:TextBox>
+        班級:<asp:TextBox ID="updateClassTextBox" runat="server"></asp:TextBox>
+        姓名:<asp:TextBox ID="updateNameTextBox" runat="server"></asp:TextBox>
+        性別:<asp:TextBox ID="updateGenderTextBox" runat="server"></asp:TextBox> 
+        生日:<asp:TextBox ID="updateBirthdayTextBox" runat="server"></asp:TextBox> 
+        <asp:Button ID="updateButton" runat="server" OnClick="updateButton_Click" Text="修改" />
+        <br />
+        <br />
+        編號:<asp:TextBox ID="deleteIDTextBox" runat="server" ></asp:TextBox>
+        <asp:Button ID="deleteButton" runat="server" OnClick="deleteButton_Click" Text="刪除" />
+        
+    
     </div>
     </form>
 </body>
--- HOMEWORK-ADO.NET-NUNIT/WebForm1.aspx.cs
+++ HOMEWORK-ADO.NET-NUNIT/WebForm1.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +14,99 @@ namespace HOMEWORK_ADO.NET_NUNIT
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            searchButton_Click(null, null);
+        }
+        protected void searchButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["StudentConnectionString"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    //cmd.CommandText = "select * from Student where Name like @name";
 
+                    cmd.CommandText = "SELECT Student_ID, Student_Class, Student_Name,Student_Gender, FORMAT(Student_Birthday,'yyyy/MM/dd') AS Student_Birthday FROM Student where Student_Name like @name;";
+
+                    cmd.Parameters.Add(new SqlParameter("@name", "%" + searchBox.Text + "%"));
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(dr);
+                        dr.Close();
+                        resultGridView.DataSource = dt;
+                        resultGridView.DataBind(); 
+
+                        //add age column and calculate age
+                 /***      dt.Columns.Add("Age", typeof(System.Int32));
+                        Calculate cal = new Calculate();
+                        foreach (DataRow data in dt.Rows)
+                        {
+                            int age = cal.getAge(Convert.ToDateTime(data["Birthday"]));
+                            data["Age"] = age;
+                        }***/
+
+                        
+                    }
+                }
+            }
+        }
+        protected void insertButton_Click(object sender, EventArgs e)
+        {
+            string sql = @"INSERT INTO [dbo].[Student]([Student_Class],[Student_Name],[Student_Gender],[Student_Birthday])
+                            VALUES (@Student_Class,@Student_Name,@Student_Gender,@Student_Birthday);SELECT CAST(scope_identity() AS int);";
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["StudentConnectionString"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Add(new SqlParameter("@Student_Class", insertClassTextBox.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Student_Name", insertNameTextBox.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Student_Gender", insertGenderTextBox.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Student_Birthday", insertBirthdayTextBox.Text));
+
+                    insertIDTextBox.Text = cmd.ExecuteScalar().ToString();
+                }
+                searchButton_Click(null, null);
+            }
+        }
+        protected void updateButton_Click(object sender, EventArgs e)
+        {
+            string sql = @"update [dbo].[Student] set [Student_Class]=@Student_Class,[Student_Name] = @Student_Name,[Student_Gender]=@Student_Gender ,[Student_Birthday]=@Student_Birthday
+                             where [Student_ID] = @Student_ID";
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["StudentConnectionString"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Add(new SqlParameter("@Student_ID", updateIDTextBox.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Student_Class", updateClassTextBox.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Student_Name", updateNameTextBox.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Student_Gender", updateGenderTextBox.Text));
+                    cmd.Parameters.Add(new SqlParameter("@Student_Birthday", updateBirthdayTextBox.Text));
+
+                    cmd.ExecuteNonQuery();
+                }
+                searchButton_Click(null, null);
+            }
+        }
+        protected void deleteButton_Click(object sender, EventArgs e)
+        {
+            string sql = @"DELETE FROM [dbo].[Student]
+                           WHERE Student_ID = @Student_ID";
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["StudentConnectionString"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+                    cmd.Parameters.Add(new SqlParameter("@Student_ID", deleteIDTextBox.Text));
+
+                    cmd.ExecuteNonQuery();
+                }
+                searchButton_Click(null, null);
+            }
         }
     }
 }
